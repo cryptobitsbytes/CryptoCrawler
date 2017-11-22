87f40d7cf1bd2252d1869d4dfbf325bcb5b3955f
Quincy <info@modernict.com>
2017-11-22 22:11:30 +0100

    Removed mongoDB stuff, made small changes to google database names / hard coded stuff.

--- config.js
+++ config.js
@@ -2,28 +2,25 @@ const nconf = require('nconf');
 const path = require('path');
 
 nconf
-  .argv()
-  .env([
-    'GCLOUD_ID',
-    'KUBERNETES_SERVICE',
-    'MONGODB_HOST',
-    'MONGODB_PORT',
-    'NODE_ENV',
-  ])
-  .file({file: path.join(__dirname, 'config.json')})
-  .defaults({
-    MONGODB_HOST: 'localhost',
-    MONGODB_PORT: '27017',
-    NODE_ENV: 'development',
-  });
-  // Check gcloud settings only when running production mode. 
-  // Only necessary for cloud deployments
+    .argv()
+    .env([
+        'GCLOUD_ID',
+        'KUBERNETES_SERVICE',
+        'NODE_ENV',
+    ])
+    .defaults({
+        NODE_ENV: 'development',
+    });
 
-  // TODO Find out why this fails on Docker. Might not be needed anyway
-  // if (nconf.get(NODE_ENV)==='production') {
-  //   checkConfig('GCLOUD_ID');
-  //   checkConfig('KUBERNETES_SERVICE');
-  // }
+// Check gcloud settings only when running production mode. 
+// Only necessary for cloud deployments
+
+// TODO Find out why this fails on Docker. Might not be needed anyway
+// if (nconf.get(NODE_ENV)==='production') {
+//   checkConfig('GCLOUD_ID');
+//   checkConfig('KUBERNETES_SERVICE');
+// }
+
 /**
  * check if the setting is set, else throuw an error
  * @param {*} setting - the setting to check
